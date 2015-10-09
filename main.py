88a7d524e9a49725bfdbf24d44370e032db3244d
Cruor99 <kliknes@gmail.com>
2015-10-09 16:56:51 +0200

    sorta done

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pyscreenshot as ImageGrab
+import datetime
+import threading
 
-im = ImageGrab.grab()
-im.show()
+
+def grabscreen():
+    datetimenow = datetime.datetime.now()
+    ImageGrab.grab_to_file("{}.png".format(str(datetimenow)))
+
+t = threading.Timer(600.0, grabscreen)
+t.start()
+
+if __name__ == "__main__":
+    grabscreen()
